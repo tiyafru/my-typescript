30c48a0e8504b8f5d24459eaf51ebeb7b0b06339
fathiyahru <fathiyah.u@weefer.co.id>
2019-12-24 15:29:34 +0700

    Isi dengan keterangan untuk commit

--- Button.ts
+++ Button.ts
@@ -1,20 +1,21 @@
 import { Button  } from '@material-ui/core';
 import { withStyles ,Theme  } from '@material-ui/core/styles';
 
-const orangeTheme = '#F17C16';
-const orangeHover = '#f09546';
-// ini ada opacity, mengikuti mockup
-const orangePale = 'rgba(241, 124, 22, 0.25)';
-const orangePaleHover = 'rgba(224, 113, 15, 0.4)';
+const color = {
+    orangeTheme: '#F17C16',
+    orangeHover: '#f09546',
+    orangePale: 'rgba(241, 124, 22, 0.25)',
+    orangePaleHover: 'rgba(224, 113, 15, 0.4)'
+  };
 
 export const ButtonSolid = 
     withStyles({
         root: {
             textTransform: 'none',
             color: 'white',
-            backgroundColor: orangeTheme,
+            backgroundColor: color.orangeTheme,
             '&:hover' : {
-                backgroundColor: orangeHover,
+                backgroundColor: color.orangeHover,
             },
         }
   })(Button);
@@ -23,10 +24,10 @@ export const ButtonPale =
     withStyles({
         root: {
             textTransform: 'none',
-            color: orangeTheme,
-            backgroundColor: orangePale,
+            color: color.orangeTheme,
+            backgroundColor: color.orangePale,
             '&:hover' : {
-                backgroundColor: orangePaleHover,
+                backgroundColor: color.orangePaleHover,
         },
     }
 })(Button);
@@ -35,6 +36,6 @@ export const ButtonReverse =
 withStyles({
         root: {
         textTransform: 'none',
-        color: orangeTheme,
+        color: color.orangeTheme,
     }
 })(Button)
