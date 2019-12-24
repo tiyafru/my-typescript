a2e4a07f7f0e9de953c75bde75d4b28db2359dad
fathiyahru <fathiyah.u@weefer.co.id>
2019-12-24 16:04:49 +0700

    Isi dengan keterangan untuk commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { ButtonPale, ButtonSolid, ButtonReverse } from './Button';
+import { PrimaryCheckbox} from './Checkbox';
+import { StandartTextField } from './Textfield';
+
+export {
+  ButtonPale,
+  ButtonSolid,
+  ButtonReverse,
+  PrimaryCheckbox,
+  StandartTextField
+}
