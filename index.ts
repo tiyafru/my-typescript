950c863b2275edf0c2d1fc8f61a0303701de9446
fathiyahru <fathiyah.u@weefer.co.id>
2019-12-26 13:58:12 +0700

    5

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
 import { ButtonPale, ButtonSolid, ButtonReverse } from './components/ButtonTheme';
 import { PrimaryCheckbox} from './components/Checkbox';
 import { StandartTextField } from './components/Textfield';
-import { Radio } from './components/Radio';
 
 export {
   ButtonPale,
   ButtonSolid,
   ButtonReverse,
   PrimaryCheckbox,
-  StandartTextField,
-  Radio
+  StandartTextField
 }
